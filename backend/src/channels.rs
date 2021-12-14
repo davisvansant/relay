b44553c2a07cc099eb16cdffeaa62ba1fe3e1e86
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-12-14 11:50:28 -0800

    add initial add_message channel impl, tests

--- backend/src/channels.rs
+++ backend/src/channels.rs
@@ -29,3 +29,65 @@ pub enum WebSocketConnection {
     SendMessage(Message),
     Close,
 }
+
+pub async fn add_message(
+    state: &StateSender,
+    message: &Message,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let (request, _response) = oneshot::channel();
+
+    state
+        .send((StateRequest::AddMessage(message.to_owned()), request))
+        .await?;
+
+    Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn add_message() -> Result<(), Box<dyn std::error::Error>> {
+        let (test_state_sender, mut test_state_receiver) =
+            mpsc::channel::<(StateRequest, oneshot::Sender<StateResponse>)>(64);
+
+        let test_task = tokio::spawn(async move {
+            let mut test_state_messages = Vec::with_capacity(5);
+
+            assert_eq!(test_state_messages.len(), 0);
+
+            while let Some((test_request, test_response)) = test_state_receiver.recv().await {
+                match test_request {
+                    StateRequest::AddMessage(test_new_message) => {
+                        test_state_messages.push(test_new_message);
+
+                        break;
+                    }
+                    StateRequest::AddUser((test_id, test_channel)) => {
+                        unimplemented!();
+                    }
+                    StateRequest::GetMessages => {
+                        unimplemented!();
+                    }
+                    StateRequest::GetUsers => {
+                        unimplemented!();
+                    }
+                    StateRequest::RemoveUser(_) => {
+                        unimplemented!();
+                    }
+                }
+            }
+
+            assert_eq!(test_state_messages.len(), 1);
+        });
+
+        let test_message = Message::text("test_message");
+
+        super::add_message(&test_state_sender, &test_message).await?;
+
+        assert!(test_task.await.is_ok());
+
+        Ok(())
+    }
+}
