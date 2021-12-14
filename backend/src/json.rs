29eaeb59baa2389218110fc758d80868deb08393
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-12-14 10:06:21 -0800

    initial message kind enum, impl

--- backend/src/json.rs
+++ backend/src/json.rs
@@ -1,1 +1,15 @@
-// unimplemented!()
+pub enum MessageKind {
+    Uuid,
+    Message,
+    ConnectedUsers,
+}
+
+impl MessageKind {
+    pub async fn build(&self) -> String {
+        match self {
+            MessageKind::ConnectedUsers => String::from("connected_users"),
+            MessageKind::Message => String::from("message"),
+            MessageKind::Uuid => String::from("uuid"),
+        }
+    }
+}
