122e2111a1246f6e2172c4c326e93e68deaf9f59
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-12-14 11:28:46 -0800

    add initial imports, types, enums

--- backend/src/channels.rs
+++ backend/src/channels.rs
@@ -1,1 +1,31 @@
-// unimplemented!()
+use std::collections::HashMap;
+use tokio::sync::{mpsc, oneshot};
+use warp::ws::Message;
+
+pub type ConnectedUsers = HashMap<String, WebSocketSender>;
+pub type StateReceiver = mpsc::Receiver<(StateRequest, oneshot::Sender<StateResponse>)>;
+pub type StateSender = mpsc::Sender<(StateRequest, oneshot::Sender<StateResponse>)>;
+pub type WebSocketReceiver = mpsc::Receiver<WebSocketConnection>;
+pub type WebSocketSender = mpsc::Sender<WebSocketConnection>;
+
+#[derive(Clone, Debug)]
+pub enum StateRequest {
+    AddMessage(Message),
+    AddUser((String, WebSocketSender)),
+    GetUsers,
+    GetMessages,
+    RemoveUser(String),
+}
+
+#[derive(Clone, Debug)]
+pub enum StateResponse {
+    Messages(Vec<Message>),
+    Users(ConnectedUsers),
+    Ok,
+}
+
+#[derive(Clone, Debug)]
+pub enum WebSocketConnection {
+    SendMessage(Message),
+    Close,
+}
