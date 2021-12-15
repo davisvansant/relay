4486a236efc1ce976bf4a4650d1bdf59c2c580a2
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-12-14 16:27:21 -0800

    initial add_user impl, tests

--- backend/src/state.rs
+++ backend/src/state.rs
@@ -27,6 +27,21 @@ impl State {
 
         Ok(())
     }
+
+    async fn add_user(
+        &mut self,
+        uuid: String,
+        websocket_sender: WebSocketSender,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        println!("checking user -> {:?}", &uuid);
+
+        match self.users.insert(uuid, websocket_sender) {
+            Some(key) => println!("updating user -> {:?}", key),
+            None => println!("adding new user..."),
+        }
+
+        Ok(())
+    }
 }
 
 #[cfg(test)]
@@ -70,6 +85,33 @@ mod tests {
 
         assert!(!test_state.messages.is_empty());
         assert_eq!(test_state.messages.len(), 1);
+
+        Ok(())
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn add_user() -> Result<(), Box<dyn std::error::Error>> {
+        let (test_state_sender, test_state_receiver) =
+            mpsc::channel::<(StateRequest, oneshot::Sender<StateResponse>)>(64);
+
+        drop(test_state_sender);
+
+        let mut test_state = State::init(test_state_receiver).await;
+
+        assert!(test_state.users.is_empty());
+        assert_eq!(test_state.users.len(), 0);
+
+        let test_uuid = uuid::Uuid::new_v4().to_string();
+        let (test_websocket_sender, test_websocket_receiver) = mpsc::channel(16);
+
+        drop(test_websocket_receiver);
+
+        test_state
+            .add_user(test_uuid, test_websocket_sender)
+            .await?;
+
+        assert!(!test_state.users.is_empty());
+        assert_eq!(test_state.users.len(), 1);
 
         Ok(())
     }
