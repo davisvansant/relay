750c153ccc6810847ce88cdeb87108b8fa7b81a8
Davis Van Sant <davisvansant@users.noreply.github.com>
2021-12-14 16:52:39 -0800

    initial get_users impl, tests

--- backend/src/state.rs
+++ backend/src/state.rs
@@ -48,12 +48,18 @@ impl State {
 
         Ok(self.messages.to_vec())
     }
+
+    async fn get_users(&self) -> ConnectedUsers {
+        self.users.clone()
+    }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::str::FromStr;
     use tokio::sync::{mpsc, oneshot};
+    use uuid::Uuid;
 
     #[tokio::test(flavor = "multi_thread")]
     async fn init() -> Result<(), Box<dyn std::error::Error>> {
@@ -144,6 +150,45 @@ mod tests {
         assert_eq!(test_messages[0].to_str().unwrap(), "test_message_one");
         assert_eq!(test_messages[1].to_str().unwrap(), "test_message_two");
         assert_eq!(test_messages[2].to_str().unwrap(), "test_message_three");
+
+        Ok(())
+    }
+
+    #[tokio::test(flavor = "multi_thread")]
+    async fn get_users() -> Result<(), Box<dyn std::error::Error>> {
+        let (test_state_sender, test_state_receiver) =
+            mpsc::channel::<(StateRequest, oneshot::Sender<StateResponse>)>(64);
+
+        drop(test_state_sender);
+
+        let mut test_state = State::init(test_state_receiver).await;
+
+        assert!(test_state.users.is_empty());
+        assert_eq!(test_state.users.len(), 0);
+
+        let test_uuid = uuid::Uuid::new_v4().to_string();
+        let (test_websocket_sender, test_websocket_receiver) = mpsc::channel(16);
+
+        test_state
+            .add_user(test_uuid, test_websocket_sender)
+            .await?;
+
+        let test_users = test_state.get_users().await;
+
+        assert!(!test_users.is_empty());
+        assert_eq!(test_users.len(), 1);
+
+        for (test_uuid, test_websocket_connection) in test_users.iter() {
+            assert_eq!(
+                Uuid::from_str(test_uuid)
+                    .expect("uuid from &str")
+                    .get_version_num(),
+                4
+            );
+
+            assert!(!test_websocket_connection.is_closed());
+            assert_eq!(test_websocket_connection.capacity(), 16);
+        }
 
         Ok(())
     }
